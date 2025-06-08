11a681862e54e024bd18d45a21f11d7ee64c9f39
david-allen-young <181532024+david-allen-young@users.noreply.github.com>
2025-06-08 14:31:53 -0700

    Create Args.h

---
+++ src/Args.h
@@ -0,0 +1,19 @@
+#pragma once
+
+enum class ColorArg
+{
+	Darkest = -2,
+	Dark = -1,
+	Neutral = 0,
+	Bright = 1,
+	Brightest = 2,
+};
+
+enum class PitchArg
+{
+	Lowest = -2,
+	Lower = -1,
+	Neutral = 0,
+	Higher = 1,
+	Highest = 2,
+};
